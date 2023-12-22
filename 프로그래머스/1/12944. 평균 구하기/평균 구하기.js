ee1f46d45dacc75b3ed8e158cb7da9dc0815b627
sekwan94 <108396364+sekwan94@users.noreply.github.com>
2023-12-22 10:23:48 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
@@ -0,0 +1,10 @@
+function solution(arr) {
+    var sum = 0;
+    
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    
+    var average = sum / arr.length;
+    return average;
+}
